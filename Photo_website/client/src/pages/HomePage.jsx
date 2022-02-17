673b807beebaf676e1389d13d43195447433f35c
Fernando <fernando.ortega79@gmail.com>
2022-02-17 15:12:35 +0100

    First commit
    Created React App
    modified CSS
    created signup / login
    created basic info text

---
+++ Photo_website/client/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+
+import "../App.css";
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Name: Claudia Matute</p>
+      <p>Contact: tuemail@tudominio.com</p>
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
