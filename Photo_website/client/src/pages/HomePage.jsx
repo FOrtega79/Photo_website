233ac189407ba5bb7d72be6ed89e3865f2bc13cf
Fernando <fernando.ortega79@gmail.com>
2022-02-17 17:09:57 +0100

    different galleries
    Everything hard coded :-1:

--- Photo_website/client/src/pages/HomePage.jsx
+++ Photo_website/client/src/pages/HomePage.jsx
@@ -1,13 +1,31 @@
 
 import "../App.css";
+import StandardImageList from "../components/ImageGallery/ImageGallery";
+
 
 function HomePage() {
   return (
     <div className="App">
+    
       <header className="App-header">
       <p>Name: Claudia Matute</p>
       <p>Contact: tuemail@tudominio.com</p>
-       
+      <p>My equipment</p>
+      <p>About me</p>
+       <h3>Landscapes</h3>
+       <div>
+       <StandardImageList />
+       </div>
+
+       <h3>B/N urban landscapes</h3>
+       <div>
+       <StandardImageList />
+       </div>
+
+       <h3>B/N Portraits</h3>
+       <div>
+       <StandardImageList />
+       </div>
         
       </header>
     </div>
