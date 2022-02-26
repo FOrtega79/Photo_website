d568c9eb1bcb11d7c273ed2a5f3d5b4be9532c6b
Fernando <fernando.ortega79@gmail.com>
2022-02-26 19:19:20 +0100

    img media

--- client/src/components/LandscapeSlide/LandscapeSlide.jsx
+++ client/src/components/LandscapeSlide/LandscapeSlide.jsx
@@ -5,20 +5,20 @@ function LandscapeSlide(){
         <div className="horizontalScroll">
             
             <div>
-                <img className="img_gallery" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645544824/Photo_site/DSC02205_wf50ra.jpg" alt="landscape2" />
+                <img className="img_gallery-vertical" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645544824/Photo_site/DSC02205_wf50ra.jpg" alt="landscape2" />
             </div>
             <div>
-                <img className="img_gallery" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645555635/Photo_site/DSC02212_vqxxx0.jpg" alt="landscape1" />
+                <img className="img_gallery-horizontal" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645555635/Photo_site/DSC02212_vqxxx0.jpg" alt="landscape1" />
             </div>
             <div>
-                <img className="img_gallery" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645544768/Photo_site/DSC02056_mdtl3t.jpg" alt="landscape1" />
+                <img className="img_gallery-horizontal" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645544768/Photo_site/DSC02056_mdtl3t.jpg" alt="landscape1" />
             </div>
             
             <div>
-                <img className="img_gallery" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645544916/Photo_site/DSC02153_ejxedk.jpg" alt="landscape1" />
+                <img className="img_gallery-horizontal" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645544916/Photo_site/DSC02153_ejxedk.jpg" alt="landscape1" />
             </div>
             <div>
-                <img className="img_gallery" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645545031/Photo_site/DSC02055_uq6rsr.jpg" alt="landscape2" />
+                <img className="img_gallery-horizontal" src="https://res.cloudinary.com/dk00a2nda/image/upload/v1645545031/Photo_site/DSC02055_uq6rsr.jpg" alt="landscape2" />
             </div>
         </div>
         
