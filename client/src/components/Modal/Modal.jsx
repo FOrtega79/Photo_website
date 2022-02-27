000a40b6c23a62e3d33f2868646712d9ca1ec5c6
Fernando <fernando.ortega79@gmail.com>
2022-02-27 15:17:50 +0100

    fonts and styling

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Link } from '@mui/material';
 
 const style = {
   position: 'absolute',
@@ -10,8 +11,8 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 350,
-  bgcolor: 'background.paper',
-  border: '1px solid #000',
+  bgcolor: 'rgba(255, 255, 255, 0.75)',
+  // border: '1px solid #000',
   boxShadow: 24,
   p: 2,
  
@@ -24,7 +25,7 @@ export default function BasicModal() {
 
   return (
     <div>
-      <Button onClick={handleOpen}>About me</Button>
+      <Link onClick={handleOpen}>About me</Link>
       <Modal
         open={open}
         onClose={handleClose}
@@ -37,10 +38,10 @@ export default function BasicModal() {
       >
         <Box className='modalBox' sx={style}>
           
-          <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+          <Typography id="modal-modal-description" sx={{ mt: 1, fontFamily:'Special Elite' }}>
           Claudia Matute is a Madrid-based photographer focused on the fashion industry.
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 1, fontSize:16 }}>
+          <Typography id="modal-modal-description" sx={{ mt: 1, fontSize:16, fontFamily:'Special Elite' }}>
           Recent clients: <br></br>
           Ynesuelves, Vogue
           </Typography>
